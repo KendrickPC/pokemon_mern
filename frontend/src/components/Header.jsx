3c6299576f6977fb31fec31a5b0b741b37044255
kendrickPC <kennethpchang@gmail.com>
2023-04-23 09:35:33 +0800

    Rating component complete

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">PTCG</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
