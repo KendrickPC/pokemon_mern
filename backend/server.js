1d6dde6f19d8ae6433ac79eb3742d7b672db18f2
kendrickPC <kennethpchang@gmail.com>
2023-04-23 21:17:08 +0800

    Modeled orderModel, productModel,and userModel schemas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
-const express = require('express')
-const products = require('./data/products')
+// const express = require('express')
+import express from 'express'
+// const dotenv = require('dotenv')
+import dotenv from 'dotenv'
+
+import connectDB from './config/database.js'
+
+// const products = require('./data/products')
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -16,4 +27,6 @@ app.get('/api/products/:id', (req, res) => {
   
 })
 
-app.listen(8080, console.log('Server running on port 8080'))
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
