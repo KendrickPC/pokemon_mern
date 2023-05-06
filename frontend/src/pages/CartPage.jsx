7bb1420521eed5653311c20ce9f301234a017f1e
kendrickPC <kennethpchang@gmail.com>
2023-05-06 16:10:39 +0800

    Add to cart functionality working

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -15,7 +15,19 @@ import {addToCart} from '../actions/cartActions'
 
 const CartPage = ({match, location, history }) => {
   const productId = match.params.id
-  if 
+  // console.log(location.search) // ?qty=2
+  const qty = location.search ? location.search.split('=')[1] : 1
+  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+  // console.log(cartItems)
+
+  useEffect( () => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
   return (
     <div>CartPage</div>
   )
