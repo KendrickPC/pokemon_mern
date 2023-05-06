17a8d14b3743ea1843c87e5bac946250be9f0666
kendrickPC <kennethpchang@gmail.com>
2023-05-06 21:19:48 +0800

    CartPage renders total quantity and total price by using reduce method

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -33,6 +33,10 @@ const CartPage = ({match, location, history }) => {
     console.log('remove')
   }
 
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
+  }
+
   return (
     <Row>
       <Col md={8}>
@@ -79,6 +83,21 @@ const CartPage = ({match, location, history }) => {
             ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+              ${cartItems.reduce( (acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
+                Proceed to Checkout 
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
     
