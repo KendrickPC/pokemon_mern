cbed26d935250587d34f08a70e952bac83ae8b99
kendrickPC <kennethpchang@gmail.com>
2023-05-08 09:13:06 +0800

    userActions stubbed up

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,15 +5,18 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers'
-
 import {
   cartReducer
 } from './reducers/cartReducers'
+import {
+  userLoginReducer
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
