608d3e47d35a1b0983392fd5436f7d10dc710dd0
kendrickPC <kennethpchang@gmail.com>
2023-05-10 04:17:58 +0800

    Order controller and route set up

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const router = express.Router()
+import {
+  addOrderItems
+} from '../controllers/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+// localhost:3000/api/orders
+router.post('/', protect, addOrderItems)
+
+export default router
