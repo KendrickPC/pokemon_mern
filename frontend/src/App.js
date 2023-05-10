20df8736463c8bf8e50b53824fafb8cd06aa650c
kendrickPC <kennethpchang@gmail.com>
2023-05-10 23:33:10 +0800

    order.totalPrice bug fixed by adding totalPrice field to schema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfilePage from './pages/ProfilePage'
 import ShippingPage from './pages/ShippingPage'
 import PaymentPage from './pages/PaymentPage'
 import PlaceOrderPage from './pages/PlaceOrderPage'
+import OrderPage from './pages/OrderPage'
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/shipping' component={ShippingPage}/>
           <Route path='/payment' component={PaymentPage}/>
           <Route path='/placeorder' component={PlaceOrderPage}/>
+          <Route path='/order/:id' component={OrderPage}/>
           
         </Container>
         
---
+++ frontend/src/pages/OrderPage.jsx
@@ -0,0 +1,136 @@
+import React, {useEffect} from 'react';
+import {
+  Button,
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Card
+} from 'react-bootstrap'
+import { Link } from 'react-router-dom/';
+
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import {getOrderDetails} from '../actions/orderActions'
+
+const OrderPage = ({match}) => {
+  const orderId = match.params.id
+  const dispatch = useDispatch()
+  const orderDetails = useSelector(state => state.orderDetails)
+  const {order, loading, error} = orderDetails
+
+  console.log("ORDER", order)
+  
+
+  if (!loading) {
+    // Fixing decimal lengths to two decimal places
+    const addDecimals = (num) => {
+      return (Math.round(num * 100) / 100).toFixed(2)
+    }
+    // Calculating item(s) price
+    order.itemsPrice = addDecimals(order.orderItems.reduce( (acc, item) => acc + item.price * item.qty, 0))
+  }
+
+  
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId))
+  }, [])
+
+  return loading ? <Loader /> : error ? 
+    <Message variant='danger'>{error}</Message> :
+    <>
+    <h1>Order {order._id}</h1>
+    <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+                {order.shippingAddress.address}, 
+                {order.shippingAddress.city}, 
+                {order.shippingAddress.postalCode}, 
+                {order.shippingAddress.country},
+              </p>
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method: </strong>
+              {order.paymentMethod}
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.length === 0  ? <Message>Order is empty</Message> : (
+                <ListGroup variant='flush'>
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image src={item.image} alt={item.name} fluid rounded />
+                        </Col>
+                        <Col>
+                          <Link to={`/products/${item.product}`}>{item.name}</Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} X {item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>Order Summary</h3>  
+            </ListGroup.Item>
+            {/* Items Price */}
+            <ListGroup.Item>
+              <Row>
+                <Col>Items</Col>
+                <Col>${order.itemsPrice}</Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Shipping Price */}
+            <ListGroup.Item>
+              <Row>
+                <Col>Shipping</Col>
+                <Col>${order.shippingPrice}</Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Tax Price */}
+            <ListGroup.Item>
+              <Row>
+                <Col>Tax</Col>
+                <Col>${order.taxPrice}</Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Total Price */}
+            <ListGroup.Item>
+              <Row>
+                <Col>Total Price</Col>
+                <Col>${order.totalPrice}</Col>
+              </Row>
+            </ListGroup.Item>
+
+            {/* Adding PayPal button here later instead of button */}
+            {/* Will also have a mark as delivered button for admins */}
+
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+
+}
+
+export default OrderPage
