da9f8183595b199a64b2afc6620c709839d38716
kendrickPC <kennethpchang@gmail.com>
2023-05-11 17:36:18 +0800

    PayPal bug not updating isPaid

--- frontend/src/pages/OrderPage.jsx
+++ frontend/src/pages/OrderPage.jsx
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import {PayPalButton} from 'react-paypal-button-v2'
 import {
   Button,
   Row,
@@ -12,21 +14,54 @@ import { Link } from 'react-router-dom/';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {getOrderDetails} from '../actions/orderActions'
+import {getOrderDetails, payOrder} from '../actions/orderActions'
+import { ORDER_PAY_RESET } from '../constants/orderConstants';
 
 const OrderPage = ({match}) => {
   const orderId = match.params.id
+  
+  const [sdkReady, setSdkReady] = useState(false)
+
   const dispatch = useDispatch()
+
   const orderDetails = useSelector(state => state.orderDetails)
   const {order, loading, error} = orderDetails
   
+  const orderPay = useSelector(state => state.orderPay)
+  const {loading: loadingPay, success: successPay} = orderPay
+  
   useEffect(() => {
+    const addPayPalScript = async () => {
+      const {data: clientId} = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
+    }
+
+
     // Check for the order and also make sure that the order ID matches the ID in the URL. 
     // If it does not, then dispatch getOrderDetails() to fetch the most recent order
-    if (!order || order._id !== orderId) {
+    if (!order || successPay || order._id !== orderId) {
+      dispatch({type: ORDER_PAY_RESET})
       dispatch(getOrderDetails(orderId))
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript()
+      } else {
+        setSdkReady(true)
+      }
     }
-  }, [dispatch, order, orderId])
+  }, [dispatch, order, orderId, successPay])
+
+  const successPaymentHandler = (paymentResult) => {
+    // console.log('paymentResult', paymentResult)
+    dispatch(payOrder(orderId, paymentResult))
+  }
 
   return loading ? <Loader /> : error ? 
     <Message variant='danger'>{error}</Message> :
@@ -59,7 +94,7 @@ const OrderPage = ({match}) => {
                 {order.paymentMethod}
               </p>
               {order.isPaid ? 
-              <Message variant='succes'>Paid on {order.paidAt}</Message> : 
+              <Message variant='success'>Paid on {order.paidAt}</Message> : 
               <Message variant='danger'>Not Paid</Message>}
             </ListGroup.Item>
             
@@ -124,6 +159,14 @@ const OrderPage = ({match}) => {
             </ListGroup.Item>
 
             {/* Adding PayPal button here later instead of button */}
+            {!order.isPaid && (
+              <ListGroup.Item>
+                {loadingPay && <Loader />}
+                {!sdkReady ? <Loader /> : (
+                  <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler}/>
+                )}
+              </ListGroup.Item>
+            )}
             {/* Will also have a mark as delivered button for admins */}
 
           </ListGroup>
