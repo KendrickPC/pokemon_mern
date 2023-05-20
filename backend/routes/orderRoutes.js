02210bac767aeda52343b767afd09f8023af6a8b
kendrickPC <kennethpchang@gmail.com>
2023-05-20 23:12:44 +0800

    user can now see their past orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,20 +3,23 @@ const router = express.Router()
 import {
   addOrderItems,
   getOrderById,
-  updateOrderToPaid
+  updateOrderToPaid,
+  getMyOrders
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // localhost:3000/api/orders
-// router.post('/', protect, addOrderItems)
-router.route('/').post(protect, addOrderItems)
+router.post('/', protect, addOrderItems)
+
+// localhost:3000/api/orders/myorders
+router.get('/myorders', protect, getMyOrders)
 
 // localhost:3000/api/orders/:id
-// router.get('/:id', protect, getOrderById)
-router.route('/:id').get(protect, getOrderById)
+router.get('/:id', protect, getOrderById)
 
 // localhost:3000/api/orders/:id/pay
-// router.put('/:id/pay', protect, updateOrderToPaid)
-router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.put('/:id/pay', protect, updateOrderToPaid)
+
+
 
 export default router
