7363c2906c787443bef7aef92b4bbfc234015b93
kendrickPC <kennethpchang@gmail.com>
2023-05-21 20:12:23 +0800

    Updating user via admin through POSTMAN working properly now

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,9 @@ import {
   registerUser,
   updateUserProfile,
   getAllUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 } from '../controllers/userController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
@@ -27,5 +29,11 @@ router.get('/', protect, isAdmin, getAllUsers)
 
 // localhost:3000/api/users/:id
 router.delete('/:id', protect, isAdmin, deleteUser)
+
+// localhost:3000/api/users/:id
+router.get('/:id', protect, isAdmin, getUserById)
+
+// localhost:3000/api/users/:id
+router.put('/:id', protect, isAdmin, updateUser)
 
 export default router
