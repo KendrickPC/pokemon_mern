e992dc12000d2a16a60f5b68b0eac31fabe07f55
kendrickPC <kennethpchang@gmail.com>
2023-05-22 03:54:41 +0800

    Admin can create new product via POSTMAN

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,17 @@ const router = express.Router()
 import {
   getProducts, 
   getProductById,
-  deleteProduct
+  deleteProduct,
+  updateProduct,
+  createProduct
 } from '../controllers/productController.js'
 import {protect, isAdmin} from '../middleware/authMiddleware.js'
+
 
 router.get('/', getProducts)
 router.get('/:id', getProductById)
 router.delete('/:id', protect, isAdmin, deleteProduct)
+router.put('/:id', protect, isAdmin, updateProduct)
+router.post('/', protect, isAdmin, createProduct)
 
 export default router
