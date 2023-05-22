aa883097fba1a27b524a60ea65976c4d7dcf8acc
kendrickPC <kennethpchang@gmail.com>
2023-05-23 02:59:53 +0800

    multer image upload configuration completed for Express

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import multer from "multer";
+import path from 'path'
+const router = express.Router()
+
+// DiskStorage documentation
+// https://www.npmjs.com/package/multer
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+// how to check/validate file type in multer
+// https://stackoverflow.com/questions/65537778/how-to-check-file-type-in-multer
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/
+  // extname will give us a Boolean if it matches filetypes
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  // has to have jpg || jpeg || png in the mimetype. Returns a Boolean
+  const mimetype = filetypes.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    cb('Images only!')
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb)
+  }
+})
+
+// Creating endpoint for image uploads
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/database.js'
@@ -5,6 +6,7 @@ import connectDB from './config/database.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -23,12 +25,29 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get('/api/config/paypal', (req, res) => 
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+// Making the "uploads" folder "static" in Express
+// Defines the __dirname__ variable and assigns it the value of the resolved absolute path of the current working directory. 
+// The path.resolve() function resolves the absolute path of the directory in which the currently executing script resides. It ensures that __dirname__ points to the correct directory, even if the script is being run from a different location.
+const __dirname__ = path.resolve()
+// Sets up a static file middleware in Express. It tells Express to serve the files in the "uploads" folder as static files, making them accessible to clients.
+// The express.static() function creates a middleware function that serves static files from the specified directory. 
+// In this case, it serves files from the "uploads" directory. The path.join() function is used to join the __dirname__ variable with the "/uploads" path segment to form the complete path to the "uploads" folder.
+app.use('/uploads', express.static(path.join(__dirname__, '/uploads')))
+// By using the above two lines of code, any files stored in the "uploads" folder will be available for access and download through the /uploads URL path in our Express application. 
+// For example, if we have a file named "pikachu.jpg" in the "uploads" folder, it can be accessed at http://yourdomain.com/uploads/pikachu.jpg
+
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
