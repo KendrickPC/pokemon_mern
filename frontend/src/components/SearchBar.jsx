f9ed7b5b0b028b3268cdd7edfbed6ee996b56a1d
kendrickPC <kennethpchang@gmail.com>
2023-05-25 17:09:42 +0800

    Searchbar component stubbed up

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {Form, Button} from 'react-bootstrap'
+
+const SearchBar = ({history}) => {
+  const [keyword, setKeyWord] = useState('')
+
+  const submitHandler = (evt) => {
+    evt.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+      <Form.Control 
+        className='mr-sm-2 ml-sm-5'
+        type='text'
+        name='q'
+        onChange={evt => setKeyWord(evt.target.value)}
+        placeholder='Search Products...'
+      ></Form.Control>
+      <Button
+        className='p-2'
+        type='submit'
+        variant='outline-success'
+      >
+        Search
+      </Button>
+    </Form>
+  )
+}
+
+export default SearchBar
